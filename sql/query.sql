4a6dec4838d35b72eb77905d07b6b13d37871b29
VuzLe <tle63917@gmail.com>
2025-07-15 18:27:53 +0700

    database

---
+++ sql/query.sql
@@ -0,0 +1,36 @@
+create database app
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    full_name VARCHAR(100) NOT NULL,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(100) NOT NULL  
+);
+
+CREATE TABLE incomes (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    description TEXT,
+    amount INT NOT NULL,
+    user_id INT NOT NULL,
+    update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_user
+        FOREIGN KEY (user_id)
+        REFERENCES users(id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE expenses (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    description TEXT,
+    amount INT NOT NULL,
+    user_id INT NOT NULL,
+    update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_user
+        FOREIGN KEY (user_id)
+        REFERENCES users(id)
+        ON DELETE CASCADE
+);
